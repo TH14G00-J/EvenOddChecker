b9a4a4a2c2f0445856d41fc788e0e3252b535ed7
Thiago <tjose.corp@gmail.com>
2025-08-21 17:13:47 -0300

    refactor: move even/odd logic from main to EvenOddChecker class
    The logic to determine if a number is even or odd was removed from the Main class
    and moved to the EvenOddChecker class. This separates responsibilities, improves code readability,
    and makes future changes, validations, and testing easier.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,19 @@
+import entities.EvenOddChecker;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        EvenOddChecker evenOddChecker = new EvenOddChecker();
 
         System.out.print("Enter a number: ");
-        int number = getValidatedIntegerInput (sc);
-
-        if(number % 2 == 0) {
-            System.out.println("Even");
-        }else {
-            System.out.println("Odd");
-        }
+        int number = getValidatedIntegerInput(sc);
+        System.out.println(evenOddChecker.getParity(number));
     }
 
-    public static int getValidatedIntegerInput (Scanner sc) {
-        while(!sc.hasNextDouble()) {
+    public static int getValidatedIntegerInput(Scanner sc) {
+        while (!sc.hasNextDouble()) {
             System.out.println("Invalid information. Please enter an integer.");
             sc.next();
         }
